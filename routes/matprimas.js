0802a133c392cee78d0cf1b54241f16f9b5b8a5c
Japsz <benjamin.meneses.14@sansano.usm.cl>
2018-11-03 01:28:33 -0300

    Rutas de crear retiro
     la vista en progreso

--- routes/matprimas.js
+++ routes/matprimas.js
@@ -31,6 +31,32 @@ router.get('/', function(req, res, next) {
 	}
 	else{res.redirect('bad_login');}	
 });
+// Enviar la vista para registrar un retiro
+router.get("/crear_retiro",function(req,res,next){
+    if(req.session.userData){
+        req.getConnection(function(err,connection){
+            if(err) console.log(err);
+            connection.query("SELECT idmaterial,codigo,detalle,stock FROM material WHERE codigo LIKE 'I%' OR codigo LIKE 'O%' OR codigo LIKE 'M%'",function (err,materiales) {
+                if(err) console.log(err);
+                res.render("matprimas/create_retiro",{mat: materiales});
+            });
+        });
+    } else res.redirect("/bad_login");
+});
+// Procesar creación de un retiro
+router.post("/save_retiro",function(req,res,next){
+    if(req.session.userData){
+        req.getConnection(function(err,connection){
+            if(err) console.log(err);
+            //req.body = { lista_m: [idmaterial1,idmaterial2],retiro_obj: { "cosas para crear el retiro" } }
+            connection.query("INSERT INTO retiro SET ?",function(err,row){
+                if(err) console.log(err);
+                if(row.length){
 
+                } else res.send({err: false,message: "No se pudo crear el retiro"});
+            });
+        });
+    } else res.redirect("/bad_login");
+});
 
 module.exports = router;
