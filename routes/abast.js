8ff59afadd7f6efeae02783d2f0e1934ad754522
Japsz <benjamin.meneses.14@sansano.usm.cl>
2018-11-27 17:53:25 -0300

    Update abast.js
    ahora si?

--- routes/abast.js
+++ routes/abast.js
@@ -1396,8 +1396,8 @@ router.get('/insumos_list/:token', function(req, res, next){
                 " LEFT JOIN movimiento_detalle ON material.idmaterial = movimiento_detalle.idmaterial" +
                 " LEFT JOIN movimiento ON movimiento_detalle.idmovimiento = movimiento.idmovimiento" +
                 " WHERE movimiento.tipo = 0 AND (recepcion.fecha" +
-                " BETWEEN '"+req.params.token.split('@')[0]+" 00:00:00' AND '"+req.params.token.split('@')[1]+" 23:59:59')" +
-				" AND (movimiento.fecha BETWEEN " + req.params.token.split('@')[0]+" 00:00:00' AND '"+req.params.token.split('@')[1]+" 23:59:59')" +
+                " BETWEEN '" + req.params.token.split('@')[0]+" 00:00:00' AND '"+req.params.token.split('@')[1]+" 23:59:59')" +
+				" AND (movimiento.f_gen BETWEEN '" + req.params.token.split('@')[0]+" 00:00:00' AND '"+req.params.token.split('@')[1]+" 23:59:59')" +
 				" GROUP BY material.idmaterial) as ingresos ON ingresos.idmaterial = salidas.idmaterial" +
 				" LEFT JOIN material on material.idmaterial=salidas.idmaterial GROUP BY material.idmaterial" ,function(err, ops){
         		if(err)
