656899b52ca2a74fed665640e9b7f9ac87c6a21c
Alniroza <alrodzam@gmail.com>
2018-12-17 18:02:47 -0300

    Se pueden agregar Clientes en Abastecimiento y ahora en MatPrimas Registrar Recepcion te muestra todas las OCA sin abastecer

--- routes/abast.js
+++ routes/abast.js
@@ -3055,6 +3055,17 @@ router.get('/get_guiaAbast/:idgd', function(req, res, next) {
     });
 });
 
+router.post('/new_cliente/', function (req, res, next) {
+    var input = JSON.parse(JSON.stringify(req.body));
+	req.getConnection(function (err, connection) {
+		connection.query("INSERT INTO cliente SET ?", input, function (err, cliente) {
+			if (err) console.log("Error en la query cliente");
+            res.send("Se inserto nuevo cliente");
+        })
+
+    });
+});
+
 
 
 module.exports = router;
