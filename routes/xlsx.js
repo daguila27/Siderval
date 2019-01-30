14aba6c0b810191ab885453c80ab665c44f0e49f
Daguila27 <daguila.gallardo@gmail.com>
2019-01-30 11:22:11 -0300

    IDS

--- routes/xlsx.js
+++ routes/xlsx.js
@@ -30,7 +30,7 @@ informe.getdatos = function(fecha,callback){
         },(...)];
         */
         connection.query("select material.codigo,material.stock,material.s_inicial,material.detalle, material.precio,material.u_medida, coalesce(facturados.facturados, 0) as sum_fact," +
-            "COALESCE(fabrs.fabricados,0) as fabricados, coalesce(peds_totales.totales) as pendientes,material.idmaterial,COALESCE(peds.solicitados,0) as solicitados,coalesce(peds_atrasados.solicitados,0) AS sol_atr" +
+            "COALESCE(fabrs.fabricados,0) as fabricados, coalesce(peds_totales.totales, 0) as pendientes,material.idmaterial,COALESCE(peds.solicitados,0) as solicitados,coalesce(peds_atrasados.solicitados,0) AS sol_atr" +
             ",COALESCE(desps.despachados,0) AS despachados,COALESCE(virts.virtuales,0) as virtuales,COALESCE(virts_oda.sum_virtual,0) as virtuales_oda" +
             ",COALESCE(necesario.neto,0) AS necesario_neto,COALESCE(rechazados.rechazados,0) AS rechazados, COALESCE(necesario.necesarios,0) AS necesarios,COALESCE(salidas_mp.sum_sal,0) as sum_sal" +
             ",coalesce(devs.sum_devs,0) as sum_dev, coalesce(ing_oda.sum_ing,0) as ing_oda FROM material" +
