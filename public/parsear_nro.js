814a87fd4242147b1ccbffcf04533b63a9759630
Daguila27 <daguila.gallardo@gmail.com>
2019-02-22 12:57:58 -0300

    table fabricaciones con footer fijo

--- public/parsear_nro.js
+++ public/parsear_nro.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 		x = $(this).text();
     	var parts = x.toString().split(".");
 	    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-	    var num =  parts.join(",");
+        var num =  parts.join(",");
 	    $(this).text(num);
 	});
 });
